c401c7d25c1a827e60ec25f4d1d2f8912de3ac66
MTalhaR <talhariaz021@gmail.com>
2022-11-09 13:37:43 +0500

    class 06

---
+++ src/com/Syntax/class06/HW1.java
@@ -0,0 +1,10 @@
+package com.Syntax.class06;
+        /*goto https://chercher.tech/practice/frames
+        1.check the checkBox
+        2.Select BabyCat from drop dwon
+        3. Send text in text box "DONE"*/
+public class HW1 {
+    public static void main(String[] args) {
+
+    }
+}
